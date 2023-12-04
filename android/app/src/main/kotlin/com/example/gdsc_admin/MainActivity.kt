dea307ffbce64550fb11b9bb03162fa354974a9f
saad <ayyansaad46@gmail.com>
2023-12-05 02:55:28 +0500

    Added icons, removed garbage, connected firebase, & added dependencies

--- android/app/src/main/kotlin/com/example/gdsc_admin/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.gdsc_admin
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/nuces/gdsc_admin/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nuces.gdsc_admin
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
